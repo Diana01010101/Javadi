01408cdc49b96e8cce2637c83d7d8f25919a457a
Diana test <diana.kapralova@mail.ru>
2023-10-11 20:42:35 +0300

    change Javadi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 public class Main {
-
     public static void main(String[] args) {
 
         int eaters = 5; // сколько людей будут есть
@@ -16,5 +15,4 @@ public class Main {
         System.out.println((spices / eaters) + " ложек(ки/ка) специй");
 
     }
-}
 }
